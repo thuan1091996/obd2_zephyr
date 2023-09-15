4d4f0614289ce5aafded0f21cf00913c4010750a
thuantm <minhthuan1091996@gmail.com>
2023-09-15 17:51:25 +0700

    driver: finish ADC driver

--- src/driver/pwm.c
+++ src/driver/pwm.c
@@ -1,18 +1,20 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/pwm.h>
+#include "common.h"
 #include "zephyr/drivers/gpio.h"
 #include "zephyr/logging/log.h"
-#include "common.h"
-
+#include "../hal.h"
 #define MODULE_NAME			        hal_pwm
-#define MODULE_LOG_LEVEL	        LOG_LEVEL_DBG
+#define MODULE_LOG_LEVEL	        LOG_LEVEL_ERR
 LOG_MODULE_REGISTER(MODULE_NAME, MODULE_LOG_LEVEL);
 
-#define PWM_DT_SPEC(n, p, i) PWM_DT_SPEC_GET_BY_IDX(n, i),
+#define PWM_DT_SPEC(node_id, prop, idx) PWM_DT_SPEC_GET_BY_IDX(node_id, idx),
 
 static struct pwm_dt_spec pwm0_device[] = {
-    DT_FOREACH_PROP_ELEM(DT_NODELABEL(pwm0_pins), pwms, PWM_DT_SPEC)} ;
+        DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), pwms, PWM_DT_SPEC)
+    } ;
+
 
 
 int __InitPWM()
@@ -121,3 +123,72 @@ int hal__read(uint8_t pinNum)
     }
     return status;
 }
+
+#include <zephyr/drivers/adc.h>
+
+#define ADC_TOTAL_CHANNELS              ARRAY_SIZE(adc_channels)
+#define ADC_DT_SPEC(node_id, prop, idx) ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
+
+static const struct adc_dt_spec adc_channels[] = {
+        DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels, ADC_DT_SPEC) 
+    };
+
+
+
+int __InitADC(void)
+{
+	/* Configure channels individually prior to sampling. */
+    for(uint8_t idx=0; idx < ARRAY_SIZE(adc_channels); idx++)
+    {
+        if (!device_is_ready(adc_channels[idx].dev))
+        {
+            LOG_ERR("ADC channel %d is not ready", idx);
+            return FAILURE;
+        }
+    }
+    LOG_INF("Total ADC channels: %d", ADC_TOTAL_CHANNELS);
+    return SUCCESS;
+}
+
+int hal__ADCRead(uint8_t channel_num)
+{
+    param_check( (channel_num >= 0) && (channel_num <ADC_TOTAL_CHANNELS));
+    if( adc_channel_setup_dt(&adc_channels[channel_num]) != 0)
+    {
+        LOG_ERR("Failed to setup ADC channel %d", channel_num);
+        return FAILURE;
+    }
+    int16_t raw_adc = 0;
+    struct adc_sequence sequence = {
+        .buffer      = &raw_adc,
+        .buffer_size = sizeof(raw_adc),
+        .channels = BIT(channel_num),
+        .resolution = adc_channels[channel_num].resolution,
+        .oversampling = adc_channels[channel_num].oversampling,
+    };
+    int err = adc_read(adc_channels[channel_num].dev, &sequence);
+    if (err != 0) {
+        LOG_ERR("ADC channel %d read failed with error %d", channel_num, err);
+        return FAILURE;
+    }
+    LOG_INF("ADC channel %d read success with value %d", channel_num, raw_adc);
+    return raw_adc;
+}
+
+int hal__ADCReadMV(uint8_t channel_num)
+{
+    int adc_mv = hal__ADCRead(channel_num);
+    if(adc_mv < 0)
+    {
+        LOG_ERR("Read ADC channel %d, reading value %d", channel_num, adc_mv);
+    }
+    int status = adc_raw_to_millivolts_dt(&adc_channels[channel_num], &adc_mv);
+    if(status != 0)
+    {
+        LOG_ERR("Failed to convert ADC channel %d raw value to mV", channel_num);
+        return FAILURE;
+    }
+    LOG_INF("ADC channel %d: %d (mV)", channel_num, adc_mv);
+    return adc_mv;
+}
+
