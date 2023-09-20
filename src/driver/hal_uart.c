4f0cd5a0b48e7e6c382c4b50b1eb0c7b85b90bf9
thuantm <minhthuan1091996@gmail.com>
2023-09-20 17:31:14 +0700

    update: re-factor UART driver

--- src/driver/hal_uart.c
+++ src/driver/hal_uart.c
@@ -25,28 +25,39 @@
 #include <devicetree.h>
 #include <drivers/uart.h>
 #include "sys/ring_buffer.h"
+#include "sys/util.h"
 
+/******************************************************************************
+* Module Preprocessor Macros
+*******************************************************************************/
 #include "logging/log.h"
 #define MODULE_NAME			        hal_uart
 #define MODULE_LOG_LEVEL	        LOG_LEVEL_DBG
 LOG_MODULE_REGISTER(MODULE_NAME, MODULE_LOG_LEVEL);
+
+#define UART_MAX_INSTANCE   ARRAY_SIZE(uart_device)
+#define UART0_NODE          DT_NODELABEL(uart0)
+#define UART1_NODE          DT_NODELABEL(uart1)
+#define UART0_BAUDRATE      DT_PROP(UART0_NODE, current_speed)
+#if DT_NODE_HAS_STATUS(UART1_NODE, okay)
+#define UART1_BAUDRATE      DT_PROP(UART1_NODE, current_speed)
+#endif
 /******************************************************************************
 * Module Preprocessor Constants
 *******************************************************************************/
-#define UART_MAX_INSTANCE               2
+
 #define UART_DEFAULT_RINGBUFFER_SIZE    64
-#define UART_DEFAULT_CONF_BAUDRATE      115200
+#define UART_DEFAULT_CONF_BAUDRATE      UART0_BAUDRATE
 #define UART_DEFAULT_CONF_PARITY        UART_CFG_PARITY_NONE
 #define UART_DEAFULT_CONF_STOP_BITS     UART_CFG_STOP_BITS_1
 #define UART_DEAFULT_CONF_DATA_BITS     UART_CFG_DATA_BITS_8
 #define UART_DEAFULT_CONF_FLOW_CTRL     UART_CFG_FLOW_CTRL_NONE
 
-#define UART_CONF_DUMP_RX_DATA          (1) // Set to 1 to dump UART RX data when received
 /******************************************************************************
-* Module Preprocessor Macros
+* Module Preprocessor Configs
 *******************************************************************************/
-#define UART0_NODE DT_NODELABEL(uart0)
-#define UART1_NODE DT_NODELABEL(uart1)
+#define UART_CONF_DUMP_RX_DATA          (1) // Set to 1 to dump UART RX data when received
+
 
 /******************************************************************************
 * Module Typedefs
@@ -60,9 +71,14 @@ typedef struct {
 /******************************************************************************
 * Module Variable Definitions
 *******************************************************************************/
-static const struct device *uart_device[UART_MAX_INSTANCE] = {
+static const struct device *uart_device[] = {
+#if DT_NODE_HAS_STATUS(UART0_NODE, okay)
 	DEVICE_DT_GET(UART0_NODE),
-	DEVICE_DT_GET(UART1_NODE),
+#endif
+
+#if DT_NODE_HAS_STATUS(UART1_NODE, okay)
+    DEVICE_DT_GET(UART1_NODE),
+#endif
 };
 
 uart_ringbuffer_t uart_ringbuffer[UART_MAX_INSTANCE];
@@ -73,7 +89,7 @@ uart_ringbuffer_t uart_ringbuffer[UART_MAX_INSTANCE];
 /******************************************************************************
 * Function Definitions
 *******************************************************************************/
-// Using LOG_HEXDUMP_DBG() to dump current ring buffer data base on head and tail member of ring buffer
+#if (UART_CONF_DUMP_RX_DATA == 1)
 void ring_buf_dump_data(struct ring_buf *p_ringbuf)
 {
     uint8_t* p_data = NULL;
@@ -109,32 +125,10 @@ void ring_buf_dump_data(struct ring_buf *p_ringbuf)
     LOG_HEXDUMP_DBG(p_data, size, "Ring buffer data");
     k_free(p_data);
 }
+#endif /* End of (UART_CONF_DUMP_RX_DATA == 1) */
 
 void uart_irq_callback(const struct device *dev, void *user_data)
 {
-    // user_data is a pointer to uart_ringbuffer_t
-    // if (!uart_irq_update(dev)) 
-    // {
-    //     LOG_ERR("Failed to update UART IRQ, device %s", dev->name);
-    //     return;
-    // }
-
-    #if 0
-    // Claim ring buffer memory
-    uart_ringbuffer_t* p_uart_ringbuffer = (uart_ringbuffer_t*)user_data;
-    uint8_t* p_ringbuf_rx = NULL;
-    uint32_t ring_buf_size = ring_buf_put_claim(&p_uart_ringbuffer->ringbuf, &p_ringbuf_rx, UART_DEFAULT_RINGBUFFER_SIZE);
-    if(ring_buf_size == 0 || p_ringbuf_rx == NULL)
-    {
-        LOG_ERR("Failed to claim ring buffer memory");
-        return;
-    }
-    // Put data into ring buffer
-    while (uart_irq_rx_ready(dev)) 
-    {
-
-	}
-    #endif
     // Claim ring buffer memory
     uart_ringbuffer_t* p_uart_ringbuffer = (uart_ringbuffer_t*)user_data;
     int rx = 0;
@@ -165,12 +159,12 @@ void uart_irq_callback(const struct device *dev, void *user_data)
     ring_buf_put_finish(&p_uart_ringbuffer->ringbuf, total_size);
 }
 
-int __UARTInit(uint8_t uart_num)
+int hal__UARTInit(uint8_t uartNum)
 {
-    param_check((uart_num >= 0) && (uart_num <= 1));
-    if( !device_is_ready(uart_device[uart_num]) )
+    param_check((uartNum >= 0) && (uartNum < UART_MAX_INSTANCE));
+    if( !device_is_ready(uart_device[uartNum]) )
     {
-        LOG_ERR("UART %d device is not ready!", uart_num);
+        LOG_ERR("UART %d device is not ready!", uartNum);
         return FAILURE;
     }
     const struct uart_config uart_cfg = {
@@ -180,34 +174,17 @@ int __UARTInit(uint8_t uart_num)
 		.data_bits = UART_DEAFULT_CONF_DATA_BITS,
 		.flow_ctrl = UART_DEAFULT_CONF_FLOW_CTRL
 	};
-    int status = uart_configure(uart_device[uart_num], &uart_cfg);
+    int status = uart_configure(uart_device[uartNum], &uart_cfg);
     if(status != 0)
     {
-        LOG_ERR("Failed to configure UART %d with error code %d", uart_num, status);
+        LOG_ERR("Failed to configure UART %d with error code %d", uartNum, status);
         return status;
     }
-    LOG_INF("Init UART %d success", uart_num);
+    LOG_INF("Init UART %d success", uartNum);
     // UART interrupt config
-    ring_buf_init(&uart_ringbuffer[uart_num].ringbuf, UART_DEFAULT_RINGBUFFER_SIZE, uart_ringbuffer[uart_num].buffer);
-    uart_irq_callback_user_data_set(uart_device[uart_num], &uart_irq_callback, &uart_ringbuffer[uart_num]);
-    uart_irq_rx_enable(uart_device[uart_num]);
-    return SUCCESS;
-}
-
-int __InitUART(void)
-{
-    int ret = __UARTInit(0);
-    if(ret != SUCCESS)
-    {
-        LOG_ERR("Failed to init UART 0");
-        return ret;
-    }
-    ret = __UARTInit(1);
-    if(ret != SUCCESS)
-    {
-        LOG_ERR("Failed to init UART 1");
-        return ret;
-    }
+    ring_buf_init(&uart_ringbuffer[uartNum].ringbuf, UART_DEFAULT_RINGBUFFER_SIZE, uart_ringbuffer[uartNum].buffer);
+    uart_irq_callback_user_data_set(uart_device[uartNum], &uart_irq_callback, &uart_ringbuffer[uartNum]);
+    uart_irq_rx_enable(uart_device[uartNum]);
     return SUCCESS;
 }
 
@@ -215,7 +192,7 @@ int __InitUART(void)
 //Returns number of bytes available to read from UART. Returns 0 on success, -1 on failure.  
 int hal__UARTAvailable(uint8_t uartNum)
 {
-    param_check((uartNum >= 0) && (uartNum <= 1));
+    param_check((uartNum >= 0) && (uartNum < UART_MAX_INSTANCE));
     int cur_size = ring_buf_size_get(&uart_ringbuffer[uartNum].ringbuf);
     if(cur_size > 0)
     {
@@ -230,7 +207,7 @@ int hal__UARTAvailable(uint8_t uartNum)
 //Write data to UART. Returns 0 on success, -1 on failure.
 int hal__UARTWrite_uint8(uint8_t uartNum, uint8_t data)
 {
-    param_check((uartNum >= 0) && (uartNum <= 1));
+    param_check((uartNum >= 0) && (uartNum < UART_MAX_INSTANCE));
     uart_poll_out(uart_device[uartNum], data);
     return SUCCESS;
 }
@@ -238,7 +215,7 @@ int hal__UARTWrite_uint8(uint8_t uartNum, uint8_t data)
 //Write data to UART. Returns number of bytes written on success, -1 on failure.
 int hal__UARTWrite(uint8_t uartNum, uint8_t *data, uint16_t len)
 {
-    param_check((uartNum >= 0) && (uartNum <= 1) && (data != NULL));
+    param_check((uartNum >= 0) && (uartNum < UART_MAX_INSTANCE)  && (data != NULL));
     for(uint16_t i = 0; i < len; i++)
     {
         uart_poll_out(uart_device[uartNum], data[i]);
@@ -249,7 +226,7 @@ int hal__UARTWrite(uint8_t uartNum, uint8_t *data, uint16_t len)
  //Read data from UART. Returns 0 on success, -1 on failure.
 int hal__UARTRead_uint8(uint8_t uartNum, uint8_t *data)
 {
-    param_check((uartNum >= 0) && (uartNum <= 1) && (data != NULL));
+    param_check((uartNum >= 0) && (uartNum < UART_MAX_INSTANCE)  && (data != NULL));
     int ret = ring_buf_get(&uart_ringbuffer[uartNum].ringbuf, data, 1);
     if(ret != 1)
     {
@@ -262,7 +239,7 @@ int hal__UARTRead_uint8(uint8_t uartNum, uint8_t *data)
  //Read data from UART. Returns number of bytes read on success, -1 on failure.
 int hal__UARTRead(uint8_t uartNum, uint8_t *data, uint16_t len)
 {
-    param_check((uartNum >= 0) && (uartNum <= 1) && (data != NULL));
+    param_check((uartNum >= 0) && (uartNum < UART_MAX_INSTANCE)  && (data != NULL));
     int ret = ring_buf_get(&uart_ringbuffer[uartNum].ringbuf, data, len);
     if(ret != len)
     {
